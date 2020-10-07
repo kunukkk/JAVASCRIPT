6646e17fad5bc28860567fd8a98775adf66a6a4b
kunukkk <showklee@naver.com>
2020-10-07 11:07:49 +0900

    add_javascript_practice
    add_javascript_practice

---
+++ 명품 JAVASCRIPT/promise.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Promise is a JavaScript object for asynchronous operation
+// State : pending -> fulfilled or rejected
+// Producer vs Consumer
+
+// 1. Producer
+// when new Promise is created, the executor runs automatically
+const promise = new Promise((resolve, reject) => {
+    // doing some heavy work (network, read files)
+    console.log('doing something...');
+    setTimeout(() => {
+        resolve('ellie');
+        reject(new Error('no network'));
+    }, 2000)
+})
+
+// 2. Consumers: the, catch, finally
+promise
+    .then(value => {
+        console.log(value);
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
